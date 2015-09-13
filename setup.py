88582cb2f07694682c37e2c04aff5478758cb9ee
Asheesh Laroia <asheesh@asheesh.org>
2015-09-12 17:07:38 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='dirtbike',
+      version='0.1',
+      description=(
+          'Convert already-installed Python modules ("distribution") to wheel'
+      ),
+      author='Asheesh Laroia',
+      author_email='asheesh@asheesh.org',
+      url='https://github.com/paulproteus/dirtbike',
+      packages=['dirtbike'],
+)
