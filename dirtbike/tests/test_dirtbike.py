c7f9c943065ba70ef2c2d944f849455ceaeb8f11
Barry Warsaw <barry@python.org>
2015-09-24 15:40:40 -0400

    Refactor the tests. Add tox dependency.

--- dirtbike/tests/test_dirtbike.py
+++ dirtbike/tests/test_dirtbike.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import glob
+import shutil
 import tempfile
 import unittest
 import subprocess
@@ -20,31 +21,25 @@ def chdir(newdir):
 
 
 class TestDirtbike(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        # Create the example wheel.  Since this source is pure-Python and
-        # compatible with both 2 & 3, create it as a universal wheel.
+    def setUp(self):
         base_dir = os.path.dirname(
             resource_filename('dirtbike.tests', '__init__.py'))
-        example_dir = os.path.join(base_dir, 'example', 'stupid')
-        cls.dist_dir = tempfile.TemporaryDirectory()
-        with chdir(example_dir):
+        self.example_dir = os.path.join(base_dir, 'example', 'stupid')
+
+    def test_sanity_check_wheel(self):
+        # Sanity check that the setUpClass() created the wheel, that it can be
+        # pip installed in a temporary directory, and that with only the
+        # installed package on sys.path, the package can be imported and run.
+        dist_dir = tempfile.TemporaryDirectory()
+        self.addCleanup(dist_dir.cleanup)
+        with chdir(self.example_dir):
             subprocess.check_call([
                 sys.executable,
                 'setup.py', '--no-user-cfg',
                 'bdist_wheel', '--universal',
-                '--dist-dir', cls.dist_dir.name,
+                '--dist-dir', dist_dir.name,
                 ])
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.dist_dir.cleanup()
-
-    def test_sanity_check(self):
-        # Sanity check that the setUpClass() created the wheel, that it can be
-        # pip installed in a temporary directory, and that with only the
-        # installed package on sys.path, the package can be imported and run.
-        wheels = glob.glob(os.path.join(self.dist_dir.name, '*.whl'))
+        wheels = glob.glob(os.path.join(dist_dir.name, '*.whl'))
         self.assertEqual(len(wheels), 1)
         wheel = wheels[0]
         with tempfile.TemporaryDirectory() as tempdir:
@@ -55,3 +50,21 @@ class TestDirtbike(unittest.TestCase):
                 env=dict(PYTHONPATH=tempdir),
                 universal_newlines=True)
         self.assertEqual(result, 'yes\n')
+
+    def test_deb_to_whl(self):
+        # Create a .deb, install it into a chroot, then turn it back
+        # into a wheel and verify the contents.
+        with chdir(self.example_dir):
+            subprocess.check_call([
+                sys.executable,
+                'setup.py', '--no-user-cfg',
+                '--command-packages=stdeb.command',
+                'bdist_deb'
+                ])
+            # bdist_deb can't be told where to leave its artifacts, so
+            # make sure that cruft gets cleaned up after this test.
+            self.addCleanup(
+                shutil.rmtree, os.path.join(self.example_dir, 'deb_dist'))
+            tar_gzs = glob.glob(os.path.join(self.example_dir, '*.tar.gz'))
+            self.assertEqual(len(tar_gzs), 1)
+            self.addCleanup(os.remove, tar_gzs[0])
