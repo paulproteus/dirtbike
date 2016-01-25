c2c602876e0df405d827704061be279f9b988ef0
Barry Warsaw <barry@python.org>
2016-01-25 14:08:23 -0500

    Bump to version 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='dirtbike',
-      version='0.1',
+      version='0.2',
       description=(
           'Convert already-installed Python modules ("distribution") to wheel'
       ),
